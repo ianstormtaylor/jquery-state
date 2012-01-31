dcef848c8fdd88c146a90a0467927fa35d68756e
Ian Storm Taylor <ian@ianstormtaylor.com>
2012-01-30 19:40:48 -0500

    added functionality for any classes, and added aliases

--- src/jquery-state.js
+++ src/jquery-state.js
@@ -1,24 +1,48 @@
 (function ($, undefined) {
 
     // Some private stuff.
-    var // Supported states.
-        _states = ['busy',
-                   'checked',
-                   'disabled',
-                   'expanded',
-                   'grabbed',
-                   'hidden',
-                   'invalid',
-                   'pressed',
-                   'readonly',
-                   'required',
-                   'selected'],
+    var // States we know about.
+        _states = ['busy'
+                 , 'checked'
+                 , 'disabled'
+                 , 'expanded'
+                 , 'grabbed'
+                 , 'hidden'
+                 , 'invalid'
+                 , 'pressed'
+                 , 'readonly'
+                 , 'required'
+                 , 'selected']
+      , _statesMatch = new RegExp(_states.join('|'))
 
-        // A regex to verify keys against.
-        _stateMatch = new RegExp(_states.join('|')),
+      // Aliases, to be forgiving.
+      , _stateAliases = ['read-only'
+                       , 'valid'
+                       , 'visible'
+                       , 'optional'
+                       , 'enabled']
+      , _stateAliasesMatch = new RegExp(_stateAliases.join('|'))
+
+      // Inverted aliases, yes its complicated.
+      , _stateInvertedAliases = ['valid'
+                               , 'visible'
+                               , 'optional'
+                               , 'enabled']
+      , _stateInvertedAliasesMatch = new RegExp(_stateInvertedAliases.join('|'))
+
+      // What do the aliases map to?
+      , _stateAliasMap = {
+            'read-only' : 'readonly'
+          , 'valid' : 'invalid'
+          , 'visible' : 'hidden'
+          , 'optional' : 'required'
+          , 'enabled' : 'disabled'
+        }
+
+
 
         // A method that changes boolean HTML attributes
-        _setBooleanAttribute = function ($element, key, value) {
+      , _setBooleanAttribute = function ($element, key, value) {
             if (value === 'true' || value === true)
                 $element.attr(key, key);
             else
@@ -60,13 +84,30 @@
         // Setting the state.
         if (value !== undefined) {
 
-            // Normalize booleans to strings.
-            if (value === true) value = 'true';
-            if (value === false) value = 'false';
+            // Normalize certain values.
+            if (value === true)
+                value = 'true';
+
+            if (value === false)
+                value = 'false';
+
+            // Allow for aliases.
+            if (key.match(_stateAliasesMatch)) {
+
+                // Use the non-alias key.
+                key = _stateAliasMap[key];
+
+                // Swap the value for inverted aliases.
+                if (key.match(_stateInvertedAliasesMatch)) {
+                    if (value === 'true')
+                        value = 'false';
+                    else if (value === 'false')
+                        value = 'true';
+                }
+            }
 
             // Does it match one of our states?
-            if (key.match(_stateMatch)) {
-
+            if (key.match(_statesMatch)) {
 
                 // =============
                 // SPECIAL CASES
@@ -101,14 +142,22 @@
                         _setBooleanAttribute(this, key, value);
                     }
                 }
-                // readonly,
+                // readonly, only for inputs and textareas (special case because of alias)
+                else if (key === 'readonly') {
+
+                    if (this.is('input, textarea')) {
+                        _setBooleanAttribute(this, key, 'read-only');
+                    }
+                }
+
                 // required, only for inputs and textareas
-                else if (key === 'readonly' || key === 'required') {
+                else if (key === 'required') {
 
                     if (this.is('input, textarea')) {
                         _setBooleanAttribute(this, key, value);
                     }
                 }
+
                 // hidden
                 else if (key === 'hidden') {
 
@@ -129,8 +178,6 @@
                         this.removeClass('mixed');
                     }
 
-                    // Add a class of mixed in addition to the checked/pressed class.
-
                 }
                 // invalid
                 else if (key === 'invalid') {
@@ -138,29 +185,31 @@
                     if (value === 'grammar' || value === 'spelling') {
                         this.addClass(key + ' ' + value);
                     }
-                    else if (key === 'invalid') {
+                    else if (value === 'false') {
                         this.removeClass('grammar spelling');
                     }
                 }
 
-
-                // =========
-                // ALL CASES
-                //
-
-                // true values
-                if (value === 'true') {
-                    this.addClass(key);
-                }
-                // false values
-                else if (value === 'false' ||
-                         value === 'undefined') {
-                    this.removeClass(key);
-                }
+                // Set the aria attribute.
+                this.attr('aria-' + key, value);
             }
 
-            // Set the aria attribute and chain.
-            return this.attr('aria-' + key, value);
+            // =========
+            // ALL CASES
+            //
+
+            // true values
+            if (value === 'true') {
+                this.addClass(key);
+            }
+            // false values
+            else if (value === 'false' ||
+                     value === 'undefined') {
+                this.removeClass(key);
+            }
+
+            // Yeh yeh my chain heavy.
+            return this;
         }
 
         // Getting the state. (via ARIA)
@@ -202,27 +251,10 @@
         var _self = this;
         $.each(args, function (i, key) {
 
-            // Does it match our states?
-            if (key.match(_stateMatch)) {
-
-                var value = _self.attr('aria-' + key);
-
-                // Important
-                if (value !== 'false' &&
-                    value !== undefined) {
-
-                    _self.state(key, false);
-                }
-                else {
-                    _self.state(key, true);
-                }
-            }
-
-            // If not, just toggle off.
-            else {
-                _self.removeState('aria-' + key);
-            }
-
+            // Does it have the class? This is how we
+            // check for true/false to know which way
+            // to toggle.
+            _self.state(key, _self.hasClass(key));
         });
 
         // Mah chain too heavy.
@@ -256,33 +288,46 @@
         var _self = this;
         $.each(args, function (i, key) {
 
-            // Remove HTML attributes.
-            if (key === 'checked'  ||
-                key === 'selected' ||
-                key === 'disabled' ||
-                key === 'readonly' ||
-                key === 'required' ||
-                key === 'hidden') {
-
-                _self.removeAttr(key);
+            // Allow for aliases.
+            if (key.match(_stateAliasesMatch)) {
+                // Use the non-alias key.
+                key = _stateAliasMap[key];
             }
 
-            // Remember to remove the non-key classes that
-            // are added to certain states above.
-            if (key === 'checked' ||
-                key === 'pressed') {
+            // Does it match one of our states?
+            if (key.match(_statesMatch)) {
 
-                _self.removeClass('mixed');
+                // Remove HTML attributes.
+                if (key === 'checked'  ||
+                    key === 'selected' ||
+                    key === 'disabled' ||
+                    key === 'required' ||
+                    key === 'hidden') {
+                    _self.removeAttr(key);
+                }
+                // Special case for readonly alias.
+                if (key === 'readonly') {
+                    _self.removeAttr('read-only');
+                }
+                // Remember to remove the non-key classes that
+                // are added to certain states above.
+                if (key === 'checked' ||
+                    key === 'pressed') {
+                    _self.removeClass('mixed');
+                }
+                if (key === 'invalid') {
+                    _self.removeClass('grammar spelling');
+                }
+
+                // Remove the aria attribute.
+                _self.removeAttr('aria-' + key);
             }
-            else if (key === 'invalid') {
 
-                _self.removeClass('grammar spelling');
-            }
-
-            // And remove the default ones finally.
-            _self.removeClass(key).removeAttr('aria-' + key);
+            // And remove the helper classes finally.
+            _self.removeClass(key);
         });
 
+        // Mah chain broke the levee.
         return this;
     };
 
